a7ce6de726061c5eaac3c2cf34eb65091ca12205
Irais Gopar osorio <iraisgoparosorio@iraiss-macbook-pro.local>
2019-01-28 14:25:25 -0800

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Irais Gopar osorio on 1/28/19.
+//  Copyright © 2019 Irais Gopar Osorio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
