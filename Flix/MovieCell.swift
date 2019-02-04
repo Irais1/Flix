1ab89f90c8c04b48d8156d4cad8fa15878c92bad
Irais Gopar osorio <iraisgoparosorio@iraiss-mbp.attlocal.net>
2019-02-03 23:26:32 -0800

    adding table rows and labels

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Irais Gopar osorio on 2/3/19.
+//  Copyright © 2019 Irais Gopar Osorio. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
