7fcb7974fa8b716ee64a6e7053e3ee1ff8405620
Irais Gopar Osorio <iraisgoparosorio@iraiss-mbp.attlocal.net>
2019-02-11 00:42:05 -0800

    Superhero complete

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Irais Gopar osorio on 2/10/19.
+//  Copyright © 2019 Irais Gopar Osorio. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
+ 
